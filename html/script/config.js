4b29d4eb9231ee181a939821b400ac692a1a73fe
TS-Shop <119107482+Toasty3920@users.noreply.github.com>
2023-03-23 21:09:58 +0100

    Create config.js

---
+++ html/script/config.js
@@ -0,0 +1,64 @@
+Config = {}
+
+Config.SoundsActivated = true
+Config.AudioVolume = 0.2
+Config.AccountnumberPrefix = "TS"
+
+// HOMEPAGE / GENERAL OPTIONS
+Config.HomepageOptions = {
+    Headline                  : "Übersicht",
+    AccountInfoHeadline       : "Kontoinformationen",
+    AccountInteractionHeadline: "Kontointeraktionen",
+    TransactionHeadline       : "Transaktionen",
+    LogoutbuttonHeadline      : "Abmelden"
+}
+
+// TRANSACTION PAGE OPTIONS
+Config.TransactionOptions = {
+    Headline: "Überweisungen"
+}
+
+// SETTINGS PAGE OPTIONS
+Config.SettingsOptions = {
+    Headline: "Einstellungen",
+    ChangeAccountnumberHeadline: "IBAN ändern",
+    ChangeAccountnumberText: "Hier kannst du deine Kontonummer in eine Andere ändern. Das Ändern deiner Kontonummer kostet dich aktuell 10.000$.",
+}
+
+// MENU BAR SETTINGS
+Config.MenuBarSettings = {
+    OverviewButtonHeadline    : "Übersicht",
+    TransactionsButtonHeadline: "Überweisungen",
+    SettingsButtonHeadline    : "Einstellungen"
+}
+
+// FASTDEPOSIT SETTINGS
+Config.FastDepositSettings = {
+    Headline: "Schnelleinzahlungen",
+    Amount1: 100,
+    Amount2: 500,
+    Amount3: 1000,
+    Amount4: 5000
+}
+
+// FASTWITHDRAW SETTINGS
+Config.FastWithdrawSettings = {
+    Headline: "Schnellauszahlungen",
+    Amount1: 100,
+    Amount2: 500,
+    Amount3: 1000,
+    Amount4: 5000
+}
+
+// KEYBOARD SETTINGS
+Config.KeyboardSettings = {
+    Deposit: {
+        Headline: "Geld einzahlen",
+    },
+    Withdraw: {
+        Headline: "Geld auszahlen",
+    },
+    Transfer: {
+        Headline: "Geld überweisen",
+    }
+}
