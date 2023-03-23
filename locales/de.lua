07e9a298b740b8ec77a7fc8de7eb865a3815ffd2
TS-Shop <119107482+Toasty3920@users.noreply.github.com>
2023-03-23 21:08:27 +0100

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,34 @@
+Locales['de'] = {
+    -- Generelle Einstellungen
+    ['blip_name'] = "Bankautomat",
+    ['bank_name'] = "Bank",
+    ['enter_atm'] = "Drücke ~INPUT_CONTEXT~ um auf den Bankautomaten zuzugreifen",
+
+    -- Geld einzahlen
+    ['notify_deposit_headline'] = "Geld eingezahlt",
+    ['notify_deposit_message']  = "Du hast ~g~%s$~w~ eingezahlt",
+    ['notify_no_deposit']       = "Einzahlung nicht möglich",
+    ['no_deposit_possible']     = "~r~Du hast nicht genug Geld!",
+
+    -- Geld auszahlen
+    ['notify_withdraw_headline'] = "Geld ausgezahlt",
+    ['notify_withdraw_message']  = 'Du hast ~g~%s$~w~ von deinem Konto ausgezahlt',
+    ['notify_no_withdraw']       = "Auszahlung nicht möglich",
+    ['no_withdraw_possible']     = "~r~Du hast nicht genug Geld!",
+
+    -- Geld überweisen
+    ['notify_transfer_headline']         = "Überweisung",
+    ['notify_transfer_message']          = 'Du hast ~g~%s$ ~w~an ~y~%s ~w~überwiesen',
+    ['notify_transfer_message_received'] = 'Du hast ~g~%s$~w~ von ~y~%s ~w~erhalten',
+    ['cant_transfer_to_yourself']        = "~r~Du kannst kein Geld an dich überweisen!",
+    ['not_enough_money_transfer']        = "Du hast nicht genug Geld um an einen Spieler Geld überweisen zu können!",
+
+    -- Kontonummer Ändern
+    ['notify_accountnumber_changed']         = "Änderung erfolgreich",
+    ['notify_accountnumber_changed_success'] = "~g~Du hast deine Kontonummer erfolgreich geändert!",
+
+    -- Fehlernachrichten Kontonummer ändern
+    ['notify_no_change']    = "Änderung fehlgeschlagen",
+    ['no_change_possible']  = "~r~Diese Kontonummer ist bereits vergeben!",
+    ['no_money_for_change'] = "~r~Du hast nicht genug Geld um deine Kontonummer zu ändern!",
+}
